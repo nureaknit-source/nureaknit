ff41aaeb78a2efff190036827fc63fc73b25cece
syabdll <106291997+4kasafa@users.noreply.github.com>
2026-07-18 23:11:54 +0800

    setup supabase

---
+++ src/proxy.ts
@@ -0,0 +1,16 @@
+import { type NextRequest } from "next/server";
+import { createClient } from "@/utils/supabase/middleware";
+
+export async function proxy(request: NextRequest) {
+  const { supabase, supabaseResponse } = createClient(request);
+
+  await supabase.auth.getClaims();
+
+  return supabaseResponse
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
